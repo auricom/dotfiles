964dcfc4c21e8d3380fbf788baa49f642e209e04
auricom <27022259+auricom@users.noreply.github.com>
2021-07-18 00:40:52 +0200

    fix: remove fish plugin done

--- run_once_after_10_fisher.sh
+++ run_once_after_10_fisher.sh
@@ -2,8 +2,6 @@
 type fisher &>/dev/null || curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install decors/fish-colored-man
 fisher install evanlucas/fish-kubectl-completions
-fisher install franciscolourenco/done
 fisher install jethrokuan/z
 fisher install patrickf3139/fzf.fish
 fisher install wfxr/forgit
-
