ce18d91ee8e9c6459fe209024554feac37bb594c
auricom <27022259+auricom@users.noreply.github.com>
2021-07-22 15:27:05 +0200

    feat: fisher module laughedelic/pisces

--- run_once_after_10_fisher.sh
+++ run_once_after_10_fisher.sh
@@ -2,6 +2,7 @@
 type fisher &>/dev/null || curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install decors/fish-colored-man
 fisher install evanlucas/fish-kubectl-completions
+fisher install laughedelic/pisces
 fisher install jethrokuan/z
 fisher install patrickf3139/fzf.fish
 fisher install wfxr/forgit
